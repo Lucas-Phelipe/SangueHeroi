c9a5693af3909df2444f95582ba5fe6e141227da
Lucas Phelipe <lucasphelipe280@gmail.com>
2024-03-05 19:31:46 -0300

    Site Sangue Herói

---
+++ index.js
@@ -0,0 +1,65 @@
+/*Klayvem Guimarães*/
+/*Lucas Phelipe*/
+/*Rodrigo Pereira Lopes*/
+/*Rafaela*/
+/*Cynthia Grayt*/
+/*Bruno Cicero*/
+
+
+const carouselList = document.querySelector('.carousel-list');
+const carouselList2 = document.querySelector('.carousel-list2');
+const nav_prototipo = document.querySelector('#acesse')
+const nav_sobre = document.querySelector('#sobre');
+
+const botaoInstagram = document.querySelector('#botaoInstagram');
+const botaoPrototipo = document.querySelector('#botaoPrototipo');
+const botaoSaibaMais = document.querySelector('#botaoSaibaMais');
+
+
+let currentIndex = 0;
+
+function showImage(index) {
+  const newTransformValue = -index * 100 + '%';
+  carouselList.style.transform = 'translateX(' + newTransformValue + ')';
+  carouselList2.style.transform = 'translateX(' + newTransformValue + ')';
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % carouselList.children.length;
+  currentIndex = (currentIndex + 1) % carouselList2.children.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + carouselList.children.length) % carouselList.children.length;
+  currentIndex = (currentIndex - 1 + carouselList2.children.length) % carouselList2.children.length;
+  showImage(currentIndex);
+}
+
+nav_sobre.addEventListener('click', () => {
+  var div = document.getElementById("end_page")
+  div.scrollIntoView({behavior: "smooth", block: "start"})
+})
+
+nav_prototipo.addEventListener('click', () => {
+  var div = document.getElementById("middle_page")
+  div.scrollIntoView({behavior: "smooth", block: "start"})
+})
+
+setInterval(nextImage, 3000); // Alterna a imagem a ada 3 segundos 
+
+function redirecionarInstagram() {
+  window.location.href = "https://www.instagram.com/sanguee_heroi/?hl=en";
+}
+
+function redirecionarProtótipo() {
+  window.location.href = "https://www.figma.com/file/IeffJhGB8PxTDoWmI33Rj6/Untitled?type=design&node-id=0%3A1&mode=design&t=LUOrt5UOdsgh0367-1";
+}
+
+function redirecionarSaibaMais() {
+  window.location.href = "https://www.google.com";
+}
+
+botaoInstagram.addEventListener("click", redirecionarInstagram);
+botaoPrototipo.addEventListener("click", redirecionarProtótipo);
+botaoSaibaMais.addEventListener("click", redirecionarSaibaMais);
